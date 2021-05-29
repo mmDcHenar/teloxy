bcc1df7be76ff0000d2cc22cf6bb6ede8753b07e
mamad-love <mamad.love.mail@gmail.com>
2021-05-29 22:02:18 +0430

    change-count-to-10

--- teloxy/views.py
+++ teloxy/views.py
@@ -4,7 +4,7 @@ from re import findall
 
 def home(req):
     cli = Tc('cli', 1029913, 'c89b062fb1b8ef18bc24a1e0c893f2ec').start() # , proxy=('socks5', '127.0.0.1', 9050))
-    msgs = cli.get_messages(cli.get_entity('hack_proxy'))
+    msgs = cli.get_messages(cli.get_entity('hack_proxy'), limit=10)
     counter = 0
     result = {}
     for msg in msgs:
