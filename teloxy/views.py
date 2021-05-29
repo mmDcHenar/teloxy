ffc1848096483be5253a4dc9c4742610b2e5ede1
mamad-love <mamad.love.mail@gmail.com>
2021-05-29 21:56:54 +0430

    fix-result

--- teloxy/views.py
+++ teloxy/views.py
@@ -6,6 +6,7 @@ def home(req):
     cli = Tc('cli', 1029913, 'c89b062fb1b8ef18bc24a1e0c893f2ec').start() # , proxy=('socks5', '127.0.0.1', 9050))
     msgs = cli.get_messages(cli.get_entity('hack_proxy'))
     counter = 0
+    result = {}
     for msg in msgs:
         proxies = findall("(?:\s|\n|)((?:https://t\.me/|tg://)proxy\?server=.+?&port=\d+?&secret=.+)(?:\s|\n|)", msg.text)
         for proxy in proxies:
