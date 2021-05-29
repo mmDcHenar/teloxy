d8073bf33370c99f4dcbe099e177521323f4cec3
mamad-love <mamad.love.mail@gmail.com>
2021-05-29 21:29:24 +0430

    add-telethon

--- teloxy/views.py
+++ teloxy/views.py
@@ -3,7 +3,7 @@ from telethon.sync import TelegramClient as Tc
 from re import findall
 
 def home(req):
-    cli = Tc('cli', 1029913, 'c89b062fb1b8ef18bc24a1e0c893f2ec') # , proxy=('socks5', '127.0.0.1', 9050))
+    cli = Tc('cli', 1029913, 'c89b062fb1b8ef18bc24a1e0c893f2ec').start() # , proxy=('socks5', '127.0.0.1', 9050))
     msgs = cli.get_messages(cli.get_entity('hack_proxy'))
     counter = 0
     for msg in msgs:
