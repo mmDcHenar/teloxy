ddd98dc12f754bb5c239378ce379e5544ac66fef
mamad-love <mamad.love.mail@gmail.com>
2021-05-29 21:22:08 +0430

    add-telethon

--- teloxy/views.py
+++ teloxy/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
+from telethon.sync import TelegramClient as Tc
+from re import findall
 
 def home(req):
-    return HttpResponse('Teloxy Web!')
+    cli = Tc('cli', 1029913, 'c89b062fb1b8ef18bc24a1e0c893f2ec') # , proxy=('socks5', '127.0.0.1', 9050))
+    msgs = cli.get_messages(cli.get_entity('hack_proxy'))
+    counter = 0
+    for msg in msgs:
+        proxies = findall("(?:\s|\n|)((?:https://t\.me/|tg://)proxy\?server=.+?&port=\d+?&secret=.+)(?:\s|\n|)", msg.text)
+        for proxy in proxies:
+            result[str(counter)] = proxy
+            counter += 1
+    cli.disconnect()
+    return JsonResponse(result)
 
