c704ece157ecd52b57aeb2b1e2de08515063fe11
mamad-love <mamad.love.mail@gmail.com>
2021-05-31 22:43:13 +0430

    test

--- teloxy/views.py
+++ teloxy/views.py
@@ -3,7 +3,12 @@ from telethon.sync import TelegramClient as Tc
 from re import findall
 
 def test(req):
-    return HttpResponse(str(req.POST))
+    try:
+        if req.GET['pass'] == 'mmdlov3':
+            res = 'nice job!'
+    except:
+        res = 'nope!'
+    return HttpResponse(res)
 
 def home(req):
     cli = Tc('cli', 1029913, 'c89b062fb1b8ef18bc24a1e0c893f2ec').start() # , proxy=('socks5', '127.0.0.1', 9050))
