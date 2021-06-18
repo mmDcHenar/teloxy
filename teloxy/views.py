c9d5d449e873796560fbe93f94f4e055dbc11420
mamad-love <mamad.love.mail@gmail.com>
2021-06-18 14:27:56 +0430

    change-proxy-method

--- teloxy/views.py
+++ teloxy/views.py
@@ -21,27 +21,27 @@ def download(req):
 
 
 def proxy(req):
-    url = "https://t.me/s/hack_proxy/"
-
+	url = "https://t.me/s/hack_proxy/"
 	channel = requests.get(url).text
 	soup = BeautifulSoup(channel, 'lxml')
 	tgpost = soup.find_all('div', class_ ='tgme_widget_message')
 	hack_proxy = []
 
 	for content in tgpost:
-        msg = content.find('div', class_ = 'tgme_widget_message_text')
-        try:
-        	msgs.append(msg.text)
-        except: pass
+		msg = content.find('div', class_ = 'tgme_widget_message_text')
+		try:
+			msgs.append(msg.text)
+		except:
+			pass
 
-    channels = [hack_proxy]
+	channels = [hack_proxy]
 
-    for channel in channels:
-        for msg in channel:
-            proxies = findall("(?:\s|\n|)((?:https://t\.me/|tg://)proxy\?server=.+?&port=\d+?&secret=.+)(?:\s|\n|)", msg)
-            for proxy in proxies:
-                if proxy not in list(result.values()):
-                    result[str(counter)] = proxy
-                    counter += 1
-    return JsonResponse(result)
+	for channel in channels:
+		for msg in channel:
+			proxies = findall("(?:\s|\n|)((?:https://t\.me/|tg://)proxy\?server=.+?&port=\d+?&secret=.+)(?:\s|\n|)", msg)
+			for proxy in proxies:
+				if proxy not in list(result.values()):
+					result[str(counter)] = proxy
+					counter += 1
+	return JsonResponse(result)
 
