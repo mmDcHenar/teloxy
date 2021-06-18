a4b182550722dc2c5e68fef47c878d4a78bc9658
mamad-love <mamad.love.mail@gmail.com>
2021-06-18 14:12:40 +0430

    change-proxy-method

--- teloxy/views.py
+++ teloxy/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse, HttpResponse
 from telethon.sync import TelegramClient as Tc
 from re import findall
+from bs4 import BeautifulSoup
+from requests import get
 
 def test(req):
     return HttpResponse('test!')
@@ -19,20 +21,27 @@ def download(req):
 
 
 def proxy(req):
-    cli = Tc('cli', 1029913, 'c89b062fb1b8ef18bc24a1e0c893f2ec').start() # , proxy=('socks5', '127.0.0.1', 9050))
-    hack_proxy = cli.get_messages(cli.get_entity('hack_proxy'), limit=10) 
+    url = "https://t.me/s/hack_proxy/"
+
+	channel = requests.get(url).text
+	soup = BeautifulSoup(channel, 'lxml')
+	tgpost = soup.find_all('div', class_ ='tgme_widget_message')
+	hack_proxy = []
+
+	for content in tgpost:
+        msg = content.find('div', class_ = 'tgme_widget_message_text')
+        try:
+        	msgs.append(msg.text)
+        except: pass
+
     channels = [hack_proxy]
-    mtp_roto = cli.get_messages(cli.get_entity('MTP_roto'), limit=10)
-    channels.append(mtp_roto)
-    counter = 0
-    result = {}
+
     for channel in channels:
         for msg in channel:
-            proxies = findall("(?:\s|\n|)((?:https://t\.me/|tg://)proxy\?server=.+?&port=\d+?&secret=.+)(?:\s|\n|)", msg.text)
+            proxies = findall("(?:\s|\n|)((?:https://t\.me/|tg://)proxy\?server=.+?&port=\d+?&secret=.+)(?:\s|\n|)", msg)
             for proxy in proxies:
                 if proxy not in list(result.values()):
                     result[str(counter)] = proxy
                     counter += 1
-    cli.disconnect()
     return JsonResponse(result)
 
